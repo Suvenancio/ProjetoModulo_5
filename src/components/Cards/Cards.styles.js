62b4b0f2f06137818cb4159dbf2f4091ffdbe0a8
Suvenancio <venancisuh@gmail.com>
2021-08-24 22:31:00 -0300

    página médicos e header

---
+++ src/components/Cards/Cards.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Card = styled.main`
+    border-radius: 20px;
+    border: 0.5px solid #5EF2D7;
+    background-color: #013A40;
+    display: flex;
+    flex-direction: row;
+    color: lavender;
+    margin: 15px;
+    >img{
+        width: 50%;
+    }
+    >div{
+        display: flex;
+    flex-direction: column;
+    }
+`
