e20cfd5707b418ecc2e75821a400bb9b23cc459d
Suvenancio <venancisuh@gmail.com>
2021-08-31 17:23:26 -0300

    implementando botão excluir e botao editar

---
+++ src/Components/Usuario/Usuario.styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+
+export const Main = styled.div`
+    border-radius: 50px;
+    border: 2px solid #5EF2D7;
+    background-color: #0E2940;
+    margin: 35px;
+    font-size: 1em;
+    max-width: 30%;
+    padding: 20px;
+    color: #5EF2D7;
+    font-size: 1em;
+    font-family: 'Varela Round', sans-serif;
+    >img{
+        width: 30%;
+    }
+
+`
+export const Container = styled.div`
+    display: flex;
+    >img{
+        width: 30%;
+        height: 10%;
+        padding: 10px;
+
+    >section{
+        display: flex;
+        flex-direction: column;
+    }
+ }  
+
+`
+export const Button = styled.div`
+    display: flex;
+    justify-content: space-around;
+
+`
